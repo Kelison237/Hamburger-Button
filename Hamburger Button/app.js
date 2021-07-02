d4124a83855351da03a5a31af9509d77f0eaa936
Kelison237 <77363856+Kelison237@users.noreply.github.com>
2021-07-02 10:20:08 -0400

    Add files via upload

---
+++ Hamburger Button/app.js
@@ -0,0 +1,12 @@
+
+let openNav = () => {
+document.querySelector("#main").style.marginLeft = "250px";
+ document.querySelector("#mySidebar").style.width = "250px";
+  }
+
+
+
+  let closeNav = () => {
+    document.querySelector("#main").style.marginLeft= "0px";
+   document.querySelector("#mySidebar").style.width = "0px";
+    }
